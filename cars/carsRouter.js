2cec91b0361342164f35d5e02227f7892d0f8915
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-10 16:47:04 -0500

    MVP

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -14,4 +14,31 @@ router.get('/', (req, res) => {
     });
   });
 
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db('carDealers').where({ id }).first()
+    .then(cars => {
+      res.json(cars);
+    }) 
+    .catch (err => {
+      res.status(500).json({ message: 'Failed to retrieve cars' });
+    });
+  });
+  
+  router.post('/', (req, res) => {
+    const carData = req.body;
+    db('carDealers').insert(carData)
+    .then(ids => {
+      db('carDealers').where({ id: ids[0] })
+      .then(newcarEntry => {
+        res.status(201).json(newcarEntry);
+      });
+    })
+    .catch (err => {
+      console.log('POST error', err);
+      res.status(500).json({ message: "Failed to store data" });
+    });
+  });
+
 module.exports = router;
